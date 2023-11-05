c78336ac49dfda6e0dd9c70cdf19d70def91d987
Md Rony <mdronyrsk01724@gmail.com>
2023-11-05 16:21:37 +0600

    all design done

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList />
+        <FromAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FromSpiltBill />
     </div>
   );
 }
@@ -56,6 +59,43 @@ function Friend({ friend }) {
           {friend.name} owes you {friend.balance}
         </p>
       )}
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FromAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>Friends Name</label>
+      <input type="text" />
+      <label>Image Url</label>
+      <input type=" text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FromSpiltBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split the bill with x</h2>
+      <label>Bill Value</label>
+      <input type="text" />
+      <label>Your Expense</label>
+      <input type="text" />
+      <label>Your Expense</label>
+      <input type="text" disabled />
+      <label>Who is paying the bill</label>
+      <select>
+        <option value="user"> you</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split Bill</Button>
+    </form>
   );
 }
